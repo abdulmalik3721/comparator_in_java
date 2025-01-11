3716adf2086dfe5b86e43fccf8c90f25c2e2de1d
ABDUL MALIK <uneeba@gmail.com>
2025-01-11 18:25:18 +0530

    map example

--- QueueRunner.java
+++ QueueRunner.java
@@ -5,20 +5,20 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Comparator;
 
-class StringLengthComparator implements Comparator<String>{
+class StringLengthComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String value1, String value2) {
 		// TODO Auto-generated method stub
 		return Integer.compare(value1.length(), value2.length());
 	}
-	
+
 }
 
 public class QueueRunner {
 	public static void main(String[] args) {
-		Queue<String> queue=new PriorityQueue<>(new StringLengthComparator());
-		queue.addAll(List.of("BANANA","APPLE","KIWI"));
+		Queue<String> queue = new PriorityQueue<>(new StringLengthComparator());
+		queue.addAll(List.of("BANANA", "APPLE", "KIWI"));
 		System.out.println(queue.poll());
 		System.out.println(queue.poll());
 		System.out.println(queue.poll());
